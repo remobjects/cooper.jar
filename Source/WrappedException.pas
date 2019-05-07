fda06a72b7801a5e0eb328fba391153e53982a35
Carlo Kok <ck@remobjects.com>
2019-05-07 07:35:02 +0200

    82523: Swift: confusion with `throw` throwing s real exception vs returning a Swift error...

---
+++ Source/WrappedException.pas
@@ -0,0 +1,22 @@
+﻿namespace com.remobjects.elements.system;
+
+type
+  WrappedException = public class(Exception)
+  public
+    constructor(aObj: Object);
+    begin 
+      inherited constructor(aObj.toString);
+      Object := aObj;
+    end;
+
+    class method Wrap(o: Object): Exception;
+    begin 
+      if o = nil then exit nil;
+      exit new WrappedException(o);
+    end;
+
+    property Object: Object; readonly;
+  end;
+
+
+end.
