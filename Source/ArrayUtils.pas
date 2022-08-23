5b84d0439e3581b2865aa26495a044dab084ecd4
marc hoffman <mh@remobjects.com>
2022-08-23 12:19:02 -0400

    Stub for ArrayUtils.getSubArray; Index and Range-based indexer properties on String

--- Source/ArrayUtils.pas
+++ Source/ArrayUtils.pas
@@ -33,6 +33,22 @@ type
         end;
       end;
     end;
+
+    [Error("ArrayUtils.getSubArray is not implemented yet.")]
+    class method getSubArray<T>(val: array of T; aStart, aLength: Integer): array of T;
+    begin
+      raise new UnsupportedOperationException("ArrayUtils.getSubArray is not implemented yet.");
+    end;
+
+    [Error("ArrayUtils.getSubArray is not implemented yet.")]
+    class method getSubArray<T>(val: array of T; aRange: Range): array of T;
+    begin
+      var lLength := length(val);
+      var lStart := aRange.fStart.GetOffset(lLength);
+      var lEnd := aRange.fEnd.GetOffset(lLength);
+      //result := getSubArray(val, lStart, lEnd-lStart);
+    end;
+
   end;
 
 implementation
---
+++ Source/String.pas
@@ -0,0 +1,22 @@
+﻿namespace RemObjects.Elements.System;
+
+type
+  String_Extension = public extension class(java.lang.String)
+  public
+
+    property Item[i: &Index]: Char read self[i.GetOffset(length)]; default;
+    property Item[r: Range]: String read getSubstringWithRange(r); default;
+
+  private
+
+    method getSubstringWithRange(aRange: Range): String;
+    begin
+      var lLength := self.length;
+      var lStart := aRange.fStart.GetOffset(lLength);
+      var lEnd := aRange.fEnd.GetOffset(lLength);
+      result := substring(lStart, lEnd); // Java String expects end, *not* length!
+    end;
+
+  end;
+
+end.
